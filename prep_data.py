0693d3e1abe793a91148ed25665823631226e839
XenonLamb <xenonlamb@gmail.com>
2019-04-01 09:36:00 -0400

    bug fix

--- prep_data.py
+++ prep_data.py
@@ -1,45 +1,62 @@
 from rdkit import Chem
 import random
+import math
 
-def gen_split(datapath):
-    suppl = Chem.SDMolSupplier(datapath)
+def gen_split(datapath, trainrate=0.8, devrate=0.9):
+    suppls = Chem.SDMolSupplier(datapath, removeHs = False, sanitize = False)
+    suppl = [x for x in suppls]
     nummols = len(suppl)
-    numtrain = int(floor(0.8 * nummols))
-    numdev = int(floor(0.9*nummols))
+    numtrain = int(math.floor(trainrate * nummols))
+    numdev = int(math.floor(devrate*nummols))
     idxs = list(range(0,nummols))
     random.shuffle(idxs)
     trainadj = []
     trainnames = []
     trainlabels = []
-    trainsmiles = []
+    #trainsmiles = []
     devadj = []
     devnames = []
     devlabels = []
-    devsmiles = []
+    #devsmiles = []
     testadj = []
     testnames = []
     testlabels = []
-    testsmiles = []
+    #testsmiles = []
     for i in range(0,numtrain):
         idx = idxs[i]
-        trainadj.append(Chem.rdmolops.GetAdjacencyMatrix(suppl[idxs[idx]], True))
-        trainnames.append([ato.GetAtomicNum() for ato in suppl[idxs[idx]].GetAtoms()])
-        trainlabels.append(suppl[idxs[idx]].GetProp("value"))
-        trainsmiles.append(Chem.MolToSmiles(suppl[idxs[idx]]))
+        if suppl[idx]==None:
+          print("No molecule at", idx)
+        else:
+          trainadj.append(Chem.rdmolops.GetAdjacencyMatrix(suppl[idx], True))
+          trainnames.append([ato.GetAtomicNum() for ato in suppl[idx].GetAtoms()])
+          trainlabels.append(suppl[idx].GetProp("value"))
+          #trainsmiles.append(Chem.MolToSmiles(suppl[idx]))
     for i in range(numtrain,numdev):
         idx = idxs[i]
-        devadj.append(Chem.rdmolops.GetAdjacencyMatrix(suppl[idxs[idx]], True))
-        devnames.append([ato.GetAtomicNum() for ato in suppl[idxs[idx]].GetAtoms()])
-        devlabels.append(suppl[idxs[idx]].GetProp("value"))
-        devsmiles.append(Chem.MolToSmiles(suppl[idxs[idx]]))
+        devadj.append(Chem.rdmolops.GetAdjacencyMatrix(suppl[idx], True))
+        devnames.append([ato.GetAtomicNum() for ato in suppl[idx].GetAtoms()])
+        devlabels.append(suppl[idx].GetProp("value"))
+        #devsmiles.append(Chem.MolToSmiles(suppl[idx]))
     for i in range(numdev,nummols):
         idx = idxs[i]
-        testadj.append(Chem.rdmolops.GetAdjacencyMatrix(suppl[idxs[idx]], True))
-        testnames.append([ato.GetAtomicNum() for ato in suppl[idxs[idx]].GetAtoms()])
-        testlabels.append(suppl[idxs[idx]].GetProp("value"))
-        testsmiles.append(Chem.MolToSmiles(suppl[idxs[idx]]))
+        testadj.append(Chem.rdmolops.GetAdjacencyMatrix(suppl[idx], True))
+        testnames.append([ato.GetAtomicNum() for ato in suppl[idx].GetAtoms()])
+        testlabels.append(suppl[idx].GetProp("value"))
+        #testsmiles.append(Chem.MolToSmiles(suppl[idx]))
 
-    return trainadj, trainnames, trainsmiles, trainlabels, devadj, devnames, devsmiles, devlabels, testadj,\
-           testnames, testsmiles, testlabels
+    return trainadj, trainnames, trainlabels, devadj, devnames, devlabels, testadj,\
+           testnames,  testlabels
 
 
+def nameArray2Dic(names):
+    """
+    input
+        names: 1d array containing atom names of 1 molecule
+    return
+        namedic: dictionary in the form of "node index":"atom name"
+    """
+    namedic = {}
+    for idx in range(0,len(names)):
+        namedic[idx] = names[idx]
+
+    return namedic
